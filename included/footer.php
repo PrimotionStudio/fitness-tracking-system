44da44b28fdd976405be8244d956442cf79ee692
The Primotion Studio <oyedelenewton@gmail.com>
2024-09-06 19:06:53 +0100

    added footer

---
+++ included/footer.php
@@ -0,0 +1,17 @@
+<footer class="footer pt-3  ">
+    <div class="container-fluid">
+        <div class="row align-items-center justify-content-lg-between">
+            <div class="col-lg-12 mb-lg-0 mb-4">
+                <div class="copyright text-center text-sm text-muted text-lg-end">
+                    © <script>
+                        document.write(new Date().getFullYear())
+                    </script>,
+                    made with &hearts; by
+                    <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Creative Tim</a> &
+                    <a href="https://primotionstudio.github.io" class="font-weight-bold" target="_blank">The Primotion Studio</a>
+                    for a better web.
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
--- sign-up.php
+++ sign-up.php
@@ -1,7 +1,9 @@
 <?php
 require_once "required/session.php";
+require_once "required/sql.php";
 const PAGE_TITLE = "Fitness Tracking System";
-require_once "included/head.php";
+require_once "func/sign-up.php";
+include_once "included/head.php";
 ?>
 
 <body class="">
@@ -9,8 +11,8 @@ require_once "included/head.php";
   <!-- Navbar -->
   <nav class="navbar navbar-expand-lg position-absolute top-0 z-index-3 w-100 shadow-none my-3 navbar-transparent mt-4">
     <div class="container">
-      <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 text-white" href="pages/dashboard.html">
-        Fitness Tracking System
+      <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 text-white" href="#">
+        <?= PAGE_TITLE ?>
       </a>
       <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon mt-2">
@@ -24,7 +26,7 @@ require_once "included/head.php";
         </ul>
         <ul class="navbar-nav d-lg-block d-none">
           <li class="nav-item">
-            <a href="sign-in" class="btn btn-sm mb-0 me-1 bg-gradient-light">Sign In</a>
+            <a href="sign-in" class="btn btn-sm mb-0 me-1 bg-gradient-light">Login</a>
           </li>
         </ul>
       </div>
@@ -38,8 +40,8 @@ require_once "included/head.php";
       <div class="container">
         <div class="row justify-content-center">
           <div class="col-lg-5 text-center mx-auto">
-            <h1 class="text-white mb-2 mt-5">Welcome to FitTrack!</h1>
-            <p class="text-lead text-white">Take charge of your health and fitness journey with FitTrack. Whether you're looking to stay active, monitor your progress, or crush your goals, we're here to help you every step of the way.</p>
+            <h1 class="text-white mb-2 mt-5">Welcome to <?= PAGE_TITLE ?>!</h1>
+            <p class="text-lead text-white">Take charge of your health and fitness journey with a <?= PAGE_TITLE ?>. Whether you're looking to stay active, monitor your progress, or crush your goals, we're here to help you every step of the way.</p>
           </div>
         </div>
       </div>
@@ -52,26 +54,23 @@ require_once "included/head.php";
               <h5>Sign Up</h5>
             </div>
             <div class="card-body">
-              <form role="form">
+              <form role="form" action="sign-up" method="post">
                 <div class="mb-3">
-                  <input type="text" class="form-control" placeholder="Name" aria-label="Name">
+                  <input type="text" class="form-control" placeholder="Name" name='name' aria-label="Name">
                 </div>
                 <div class="mb-3">
-                  <input type="email" class="form-control" placeholder="Email" aria-label="Email">
+                  <input type="email" class="form-control" placeholder="Email" name='email' aria-label="Email">
                 </div>
                 <div class="mb-3">
-                  <input type="password" class="form-control" placeholder="Password" aria-label="Password">
+                  <input type="password" class="form-control" placeholder="Password" name='password' aria-label="Password">
                 </div>
-                <div class="form-check form-check-info text-start">
-                  <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
-                  <label class="form-check-label" for="flexCheckDefault">
-                    I agree the <a href="javascript:;" class="text-dark font-weight-bolder">Terms and Conditions</a>
-                  </label>
+                <div class="mb-3">
+                  <input type="password" class="form-control" placeholder="Password" name='confirm_password' aria-label="Password">
                 </div>
                 <div class="text-center">
-                  <button type="button" class="btn bg-gradient-dark w-100 my-4 mb-2">Sign up</button>
+                  <button type="submit" class="btn bg-gradient-dark w-100 my-4 mb-2">Sign up</button>
                 </div>
-                <p class="text-sm mt-3 mb-0">Already have an account? <a href="javascript:;" class="text-dark font-weight-bolder">Sign in</a></p>
+                <p class="text-sm mt-3 mb-0">Already have an account? <a href="sign-in" class="text-dark font-weight-bolder">Sign in</a></p>
               </form>
             </div>
           </div>
